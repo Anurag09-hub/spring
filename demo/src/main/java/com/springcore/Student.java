0b23b1fde02dd323558ba0a3a8096de7ea1dd31c
Anurag Singh <anuragsingh.dev09@gmail.com>
2025-08-03 15:24:12 +0530

    updates

---
+++ demo/src/main/java/com/springcore/Student.java
@@ -0,0 +1,42 @@
+package com.springcore;
+public class Student {
+    private int studentId;
+    private String studentName;
+    private String studentAddress;
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    public void setStudentAddress(String studentAddress) {
+        this.studentAddress = studentAddress;
+    }
+    public int getStudentId() {
+        return studentId;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public String getStudentAddress() {
+        return studentAddress;
+    }
+    public Student(int studentId, String studentName, String studentAddress) {
+        this.studentId = studentId;
+        this.studentName = studentName;
+        this.studentAddress = studentAddress;
+    }
+
+    public Student() {
+        super();
+        // Default constructor
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentId=" + studentId + ", studentName=" + studentName + ", studentAddress=" + studentAddress
+                + "]";
+    }
+      
+}
